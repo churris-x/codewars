f94e5159be274943fcbc3a4ef59e1a9e0c512891
Fran <francisco@altimari.nl>
2022-05-21 17:51:42 +0200

    Complete 2 steps of rotateAndRemove

--- rotateAndRemove.js
+++ rotateAndRemove.js
@@ -1,8 +1,8 @@
 /*
 	Battle plan:
 	- remove 1 of the lowest and 1 of the highest elements in an array 	[x]
-	- rotate 3x3 array of array											[ ]
-	- rotate nxn														[ ]
+	- rotate 3x3 array of array											[x]
+	- rotate nxn														[x]
 	- rotate and remove nxn -> (n-2)x(n-2)								[ ]
 	- at the end 3x3 -> 3x1 -> [x, y, z] -> x							[ ]
 */
@@ -10,5 +10,20 @@
 
 const remove = array => array.sort((a, b) => a - b).slice(1, -1);
 
-console.log(remove([5, 8, 4, 5, 6])); // [5, 5, 6]
-console.log(remove([9, 2, 5, 8, 5, 2, 9])); // [2, 5, 5, 8, 9]
+// console.log(remove([5, 8, 4, 5, 6])); // [5, 5, 6]
+// console.log(remove([9, 2, 5, 8, 5, 2, 9])); // [2, 5, 5, 8, 9]
+
+const square = [
+	[5, 5, 5],
+	[3, 5, 4],
+	[4, 6, 3],
+];
+
+const rotate = square => square.reduce((newSquare, currRow, index, array) => {
+
+	if (!index) return [...Array(array.length).keys()].map((row, i) => [array[index][i]]);
+
+	return newSquare.map((row, i) => [...row, array[index][i]]);
+}, []);
+
+console.log(rotate(square));
