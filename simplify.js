1f2cdd7c674b73eeaccb46b12bafa730813c12dd
Fran <francisco@altimari.nl>
2022-05-21 16:51:57 +0200

    Add new solution to simplify

--- simplify.js
+++ simplify.js
@@ -120,7 +120,7 @@ const simplify1 = input => {
 	return sum[0] === '+' ? sum.substring(1) : sum;
 };
 
-const simplify = input => {
+const simplify2 = input => {
 
 	const poly = '+-'.includes(input[0]) ? input : '+'.concat(input);
 
@@ -188,27 +188,67 @@ const simplify = input => {
 		return [...previous, current];
 	});
 
-	console.log(poly)
-	console.log(terms)
+	// console.log(poly)
+	// console.log(terms)
 	return sum[0] === '+' ? sum.substring(1) : sum;
 };
 
+const simplify = poly => {
+	// get number or variable from a term respectively
+	const getNum = term => [...term].filter(i => '+-0123456789'.includes(i)).join('');
+	const getVar = term => [...term].filter(i => !'+-0123456789'.includes(i)).sort().join('');
 
-// console.log(simplify("-a+5ab+3a-c-2a+8"));
-// console.log(simplify("+7b-3aby+9ayb+12b-1b-3b-3ba+9ab"));
-console.log(simplify("+1xza-4xz-4xz-2zx-12x+9x+12zx-1xz+4x"));
-// console.log(simplify("+2axy+13y+1xy-4y+7y-3axy"));
-// console.log(simplify("dc+dcba"));
-// console.log(simplify("2xy-yx"));
-// console.log(simplify("-abc+3a+2ac"));
-// console.log(simplify("xzy+zby"));
-// console.log(simplify("a+2a+4a-10a"));
-// console.log(simplify("a+2a+4a-8+12"));
-// console.log(simplify("2xy-4yx"));
+	const parse = num => {
+		switch (num) {
+			case '+': return '1';
+			case '-': return '-1';
+			case 1: return '+';
+			case -1: return '-';
+			default: return num > 1 ? `+${Number(num)}` : num;
+		}
+	};
+
+	// Add leading + sign: '+3a-5b'
+	const exp = `${'+-'.includes(poly[0]) ? '' : '+'}`.concat(poly)
+		// Add space before +- and split into terms: ['+3a', '-5ab']
+		.split('').map(i => '+-'.includes(i) ? ` ${i}` : i).join('').substring(1).split(' ')
+		// Reduce array into object, adding up equal variables: {x: 3, ab: -5}
+		.reduce((poly, term, index, array) => {
+			const isLast = index === array.length - 1;
+			const char = getVar(term);
+			const number = Number(parse(getNum(term)));
+
+			const newPoly = { ...poly, [char]: number + (poly[char] || 0) };
+
+			// If last item, convert to array: [[a, 3], [ab, -5]]
+			if (isLast) return Object.entries(newPoly)
+			return newPoly;
+		}, {})
+		// Sort terms a > c > abc > xyz
+		.sort(([aChar], [bChar]) => aChar.length - bChar.length || aChar.localeCompare(bChar))
+		// Filter out variables with 0 and parse +1 / -1 into + / - : ['+3a', '-5ab']
+		.map(([char, num]) => !num ? '' : `${parse(num)}${char}`)
+		// '+3a-5ab'
+		.join('');
+
+	// Remove leading + 
+	return exp[0] === '+' ? exp.substring(1) : exp;
+	// return exp.substring(exp[0] === '+');
+};
+
+console.log(simplify("-a+5ab+3a-c-2a+8"), simplify2("-a+5ab+3a-c-2a+8"));
+console.log(simplify("+7b-3aby+9ayb+12b-1b-3b-3ba+9ab"), simplify2("+7b-3aby+9ayb+12b-1b-3b-3ba+9ab"));
+console.log(simplify("+1xza-4xz-4xz-2zx-12x+9x+12zx-1xz+4x"), simplify2("+1xza-4xz-4xz-2zx-12x+9x+12zx-1xz+4x"));
+console.log(simplify("+2axy+13y+1xy-4y+7y-3axy"), simplify2("+2axy+13y+1xy-4y+7y-3axy"));
+console.log(simplify("dc+dcba"), simplify2("dc+dcba"));
+console.log(simplify("2xy-yx"), simplify2("2xy-yx"));
+console.log(simplify("-abc+3a+2ac"), simplify2("-abc+3a+2ac"));
+console.log(simplify("xzy+zby"), simplify2("xzy+zby"));
+console.log(simplify("a+2a+4a-10a"), simplify2("a+2a+4a-10a"));
+console.log(simplify("a+2a+4a-8+12"), simplify2("a+2a+4a-8+12"));
+console.log(simplify("2xy-4yx"), simplify2("2xy-4yx"));
 
 // if ([0,1,2,3,4,5,6,7,8,9].indexOf(event.keyCode) >= 0) {
-// 	event.preventDefault();
-// 	this.handleKey(event.key);
 // }
 
 // {
@@ -216,3 +256,8 @@ console.log(simplify("+1xza-4xz-4xz-2zx-12x+9x+12zx-1xz+4x"));
 // 	sign
 // 	count
 // }
+/*
+	terms
+	5x + 3
+	Coeficient variable, constant
+*/
