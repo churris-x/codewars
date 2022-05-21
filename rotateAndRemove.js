854c06f816427beadc83ddb69584e5df8e6013db
Fran <francisco@altimari.nl>
2022-05-21 18:01:13 +0200

    Made rotation counter clockwise

--- rotateAndRemove.js
+++ rotateAndRemove.js
@@ -3,6 +3,7 @@
 	- remove 1 of the lowest and 1 of the highest elements in an array 	[x]
 	- rotate 3x3 array of array											[x]
 	- rotate nxn														[x]
+	- invert rotation													[x]
 	- rotate and remove nxn -> (n-2)x(n-2)								[ ]
 	- at the end 3x3 -> 3x1 -> [x, y, z] -> x							[ ]
 */
@@ -18,12 +19,20 @@ const square = [
 	[3, 5, 4],
 	[4, 6, 3],
 ];
+const largeSquare = [
+	[3, 5, 8, 4, 2],
+	[1, 9, 2, 3, 8],
+	[4, 6, 7, 2, 2],
+	[7, 5, 5, 5, 5],
+	[6, 5, 3, 8, 1]
+]
 
 const rotate = square => square.reduce((newSquare, currRow, index, array) => {
 
-	if (!index) return [...Array(array.length).keys()].map((row, i) => [array[index][i]]);
+	if (!index) return [...Array(array.length).keys()].map((row, i) => [array[index][array.length - 1 - i]]);
 
-	return newSquare.map((row, i) => [...row, array[index][i]]);
+	return newSquare.map((row, i) => [...row, array[index][array.length - 1 - i]]);
 }, []);
 
 console.log(rotate(square));
+console.log(rotate(largeSquare));
