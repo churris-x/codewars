49485b85725d09f74e3d91261b15fd234cbf3150
Fran <francisco@altimari.nl>
2022-05-23 17:23:12 +0200

    Add rgba function

--- rgb.js
+++ rgb.js
@@ -1,6 +1,19 @@
-const rgb = (r, g, b) => [r, g, b].map(
+const rgb0 = (r, g, b) => [r, g, b].map(
 	byte => Math.max(0, Math.min(255, byte)).toString(16).padStart(2, '0').toUpperCase()
 ).reduce((hex, byte) => hex + byte);
+
+const rgb1 = (r, g, b) => [r, g, b].map(
+	byte => Math.max(0, Math.min(255, byte)).toString(16).padStart(2, '0').toUpperCase()
+).join('');
+
+const rgb = (...args) => args.map(
+	byte => Math.max(0, Math.min(255, byte)).toString(16).padStart(2, '0').toUpperCase()
+).reduce((hex, byte) => hex + byte, '#');
+
+const rgba = (r, g, b, a) => [r, g, b, Math.round(a * 255)].map(
+	byte => Math.max(0, Math.min(255, byte)).toString(16).padStart(2, '0').toUpperCase()
+).reduce((hex, byte) => hex + byte, '#');
+
 
 console.log(rgb(33, 234324, -54343))
 console.log(rgb(5, 201, 118))
