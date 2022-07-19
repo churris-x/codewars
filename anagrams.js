e0b600acfd1b50e1b9cb6e6e324f4780b2b7f080
Fran <francisco@altimari.nl>
2022-07-19 12:14:43 -0300

    Refactor anagrams

--- anagrams.js
+++ anagrams.js
@@ -3,16 +3,10 @@ const anagrams0 = (word, variations) => {
 	return variations.filter(a => a !== word && [...a].sort().join('') === sortWord);
 };
 
-const anagrams = (word, variations) => variations.filter(a => a.length === word.length && [...a].sort().join('') === [...word].sort().join(''));
-
-const anagrams2 = (word, variations) => {
-	const sortWord = [...word];
-
-	for (const letter of sortWord) {
-
-	}
-	return variations.filter(a => a !== word && [...a].sort().join('') === sortWord);
-};
+const anagrams = (word, variations) => variations.filter(a =>
+	a.length === word.length &&
+	[...a].sort().join('') === [...word].sort().join('')
+);
 
 console.log(anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']));
 console.log(anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']));
