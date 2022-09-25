3089be7dae7baaceed8d2ebd2f1804dcea3eadac
Fran <francisco@altimari.nl>
2022-09-25 23:28:26 +0200

    Solve Persistence kata

--- persistence.js
+++ persistence.js
@@ -2,7 +2,8 @@
 	Battle plan: 
 	- [x] count digits, if > 1 return
 	- [x] multiply digits, reduce
-	- [ ] meta reduce?	
+	- [x] recurse
+	- [ ] count how many times
 
 	E.g.: 
 		39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit)
@@ -11,7 +12,24 @@
 */
 
 
-const persistence = number => [...`${number}`].reduce((prev, curr) => prev * curr, 1 );
+const persistence0 = (number, count = 0) => {
+	const multiply = n => [...`${n}`].reduce((prev, curr) => prev * curr, 1 )
+	const newNumber = multiply(number);
+
+	console.log('new number', newNumber)
+	
+	return `${number}`.length > 1 ? persistence(newNumber, count+1) : count;
+};
+
+const persistence1 = (number, count = 0) => `${number}`.length > 1 
+	? persistence([...`${number}`].reduce((prev, curr) => prev * curr, 1 ), count+1)
+	: count;
+
+const persistence = (number, count = 0) => `${number}`.length > 1 
+	? persistence([...`${number}`].reduce((product, factor) => product * factor, 1 ), count + 1)
+	: count;
+
+
 
 console.log('2', persistence(2));
 console.log('43', persistence(43));
