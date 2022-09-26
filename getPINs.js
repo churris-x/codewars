ecdb515ad590951c39013fc710d48bd7433d1f79
Fran <francisco@altimari.nl>
2022-09-26 11:04:49 +0200

    Basic soltution for getPins

--- getPINs.js
+++ getPINs.js
@@ -15,6 +15,10 @@ how to organize the un symetrical relationship between possible outcomes?
 
 also a combinatorics issue (n chose k), can I do something with binary?
 
+
+	- [ ] solve again without using flatmap
+	- [ ] solve in the stupid order the kata prefers
+
 eg:
     '8': ['5', '7', '8', '9', '0']
     
@@ -22,6 +26,26 @@ eg:
 */
 
 // const getNumbers = pin => [...pin].map(n => numbers[Number(n)].map(i => i));
+
+// const getPins0 = pin => {
+// 	const numbers = [
+// 		[0, 8], [1, 2, 4], [2, 1, 3, 5], [3, 2, 6], [4, 1, 5, 7],
+// 		[5, 2, 4, 6, 8], [6, 3, 5, 9], [7, 4, 8], [8, 0, 5, 7, 9], [9, 6, 8],
+// 	];
+
+// 	const possibleKeys = [...pin].map(n => numbers[Number(n)]);
+// 	const totalComb = possibleKeys.reduce((product, current) => product * current.length, 1);
+
+// 	const combinations = [...Array(totalComb).keys()].map(i => possibleKeys
+// 		.reduce((s, c) => {
+// 			console.log(c[~~(i/(totalComb/c.length))])
+// 			return s.concat(c[ ~~(i/(totalComb/c.length))  ])
+// 		}, [])
+// 	);
+
+// 	return combinations;
+// };
+
 
 const getPins = pin => {
 	const numbers = [
@@ -38,20 +62,16 @@ const getPins = pin => {
 	];
 
 	const possibleKeys = [...pin].map(n => numbers[Number(n)]);
-	const totalComb = possibleKeys.reduce((product, current) => product * current.length, 1);
 
-	const combinations = [...Array(totalComb).keys()].map(i => possibleKeys
-		.reduce((s, c) => {
-			console.log(c[~~(i/(totalComb/c.length))])
-			return s.concat(c[ ~~(i/(totalComb/c.length))  ])
-		}, [])
-	);
+	const calc = (comb = [''], values) => values.length ? calc(comb.flatMap(i => values[0].map(j => i + j)), values.slice(1)) : comb;
 
-	return combinations;
+	return calc([''], possibleKeys);
 };
 
+
+
 // console.log(getPins('8'));
+// console.log(getPins('10'));
 // console.log(getPins('11'));
-console.log(getPins('10'));
-// console.log(getPins('010'));
+console.log(getPins0('369'));
 
